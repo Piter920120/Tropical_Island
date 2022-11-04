184dea3a4e8de2a795f94cf8490c0a6d20100a1e
LENOVO <piotrrrynkowskii@gmail.com>
2022-11-04 13:02:01 +0100

    adding JS to burgerBtn menu which changes his colors

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector('.nav');
 const navBtn = document.querySelector('.burger-btn');
 const allNavItems = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active');
+
+navBtnBars.classList.remove('black-bars-color')
 
 	allNavItems.forEach((item) => {
 		item.addEventListener('click', () => {
@@ -16,7 +21,7 @@ const handleNav = () => {
 const handleNavAnimation = () => {
 	let delayTime = 0;
 
-	allNavItems.forEach(item => {
+	allNavItems.forEach((item) => {
 		item.classList.toggle('nav-items-animation');
 
 		item.style.animationDelay = '.' + delayTime + 's';
@@ -24,4 +29,25 @@ const handleNavAnimation = () => {
 	});
 };
 
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerHTML = year;
+};
+
+const handleObserver = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach(section => {
+
+		if(section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+			navBtnBars.classList.add('black-bars-color')
+		}else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60){
+			navBtnBars.classList.remove('black-bars-color')
+		}
+
+	})
+};
+
+handleCurrentYear();
 navBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver)
